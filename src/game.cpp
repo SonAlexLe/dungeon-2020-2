315f9341d5b0bd33d4535441118148d6b780710a
Talsta Leo <talstal1@lyta.org.aalto.fi>
2020-07-26 16:53:20 +0300

    Cleaned up code

--- src/game.cpp
+++ src/game.cpp
@@ -9,33 +9,33 @@ Game::~Game()
 void Game::init() //add arguments?? 
 {
     //game should also open all required sprites to memory, throw errors if files are not found.
-    sf::RenderWindow    window(sf::VideoMode(800, 600),    "TEST GAME");
-    dungeon_    =   Map();
-    score_  =   0;
-    difficulty_ =   0;
-    p1_     =   Player();
+    sf::RenderWindow window(sf::VideoMode(800, 600), "TEST GAME");
+    dungeon_ = Map();
+    score_ = 0;
+    difficulty_ = 0;
+    p1_ = Player();
     window_ = &window;
     clock_.restart();
 }
 
-void    Game::input()
+void Game::input()
 {
     sf::Event event;
     while(window_->pollEvent(event))
     {
         switch(event.type)
         {
-            case    sf::Event::Closed:
+            case sf::Event::Closed:
             //handle window close
                 window_->close();
-                isRunning_  =    false;
+                isRunning_ = false;
                 break;
 
-            case    sf::Event::KeyPressed: 
+            case sf::Event::KeyPressed: 
             //pass keyboard input to the player object, needs to be updated if second player is implemented
             //note, the event only handles key down / key up, meaning that pressing a key must start the player moving
             //until the key is released, ie. the key up event is passed to the player!! 
-                if(event.key.code   ==  sf::Keyboard::Escape)
+                if(event.key.code == sf::Keyboard::Escape)
                 {
                     //code to pause the game
                     //pausing should stop the game from rendering and stop the game logic, making only a pause menu render.
@@ -47,15 +47,15 @@ void    Game::input()
                 }
                 break;
 
-            case    sf::Event::KeyReleased:
+            case sf::Event::KeyReleased:
                 p1_.input(event.key);
                 break;
 
-            case    sf::Event::MouseButtonPressed:
+            case sf::Event::MouseButtonPressed:
                 p1_.click(event);
                 break;
 
-            case    sf::Event::MouseButtonReleased:
+            case sf::Event::MouseButtonReleased:
                 p1_.release(event);
                 break;
 
@@ -67,20 +67,20 @@ void    Game::input()
 
 void    Game::update()
 {
-    for(auto i  :    p1_.GetRoom().GetDenizens())
+    for(auto i : p1_.GetRoom().GetDenizens())
     {
-        i.update(clock_.getElapsedTime()    -   lastUpdate_);
+        i.update(clock_.getElapsedTime() - lastUpdate_);
     }
-    p1_.update(clock_.getElapsedTime()  -   lastUpdate_);
+    p1_.update(clock_.getElapsedTime() - lastUpdate_);
     
-    lastUpdate_ =   clock_.getElapsedTime();
+    lastUpdate_ = clock_.getElapsedTime();
 
     //go through all the active entities in the current room and move them up to their speed.
     //enemy AI should happen here
     //check fore entity & projectile collision
     //
 } 
-void    Game::render()
+void Game::render()
 {
     window_->clear();
 
@@ -88,7 +88,7 @@ void    Game::render()
     window_->display();
 }
 
-void    Game::clean()
+void Game::clean()
 {
     //memory cleanup on game close, will be created later...
 }
--- src/game.hpp
+++ src/game.hpp
@@ -12,28 +12,28 @@ class   Game
 public:
     Game();
     //Game cannot be copied
-    Game(const Game&)   =   delete;
-    Game    operator=(const Game&) =   delete;
+    Game(const Game&) = delete;
+    Game operator=(const Game&) = delete;
     ~Game();
 
-    void     init();
-    void    input();
-    void    update();
-    void    render();
-    void    clean();
-    bool    isRunning();
+    void init();
+    void input();
+    void update();
+    void render();
+    void clean();
+    bool isRunning();
 
 
 private:
-    bool    isRunning_;
+    bool isRunning_;
      
     int score_;
     int difficulty_;
 
-    sf::RenderWindow    *window_;
+    sf::RenderWindow *window_;
     Map dungeon_;
-    Player  p1_;
-    sf::Clock   clock_;
-    sf::Time    lastUpdate_;
+    Player p1_;
+    sf::Clock clock_;
+    sf::Time lastUpdate_;
     //add possible additional player...
 };

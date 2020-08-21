5b768ba37297038a74f3b8b22b3f86f3c5d059df
rashid <rashid.jalloh@aalto.fi>
2020-08-21 16:02:47 +0300

    pulled updated monsters

--- src/monster.cpp
+++ src/monster.cpp
@@ -1,16 +1,30 @@
-#pragma once
 #include "monster.hpp"
+#include <iostream>
+#define ORC_HP 100
+#define ORC_SPEED 20
 
-Monster::Monster() {}
+Monster::Monster(float x, float y, Room* room, sf::Vector2f velocity, int hp)
+    : room_(room), velocity_(velocity), hp_(hp), Entity(x, y) {}
 
 const std::string Monster::GetSpriteName() const { return "monster.png"; }
 
 void Monster::load() {
     sf::Texture texture_;
-    texture_.loadFromFile(GetSpriteName(), sf::IntRect(currPos_.x, currPos_.y, 10, 10));
+    texture_.loadFromFile(GetSpriteName(), sf::IntRect(currPos_.x, currPos_.y, ORC_SPEED, ORC_SPEED));
     sprite_.setTexture(texture_);
 }
 
-void Monster::update(sf::Time dt) {
-    currPos_ += velocity_;
+int Monster::GetHP() { return hp_; }
+
+void Monster::SetHP(int hp) { hp_ = hp; }
+
+Orc::Orc(float x, float y, Room* room) : Monster(x, y, room, sf::Vector2f(ORC_SPEED, ORC_SPEED), ORC_HP) {}
+
+void Orc::update(sf::Time dt) {
+    if(currPos_.x >= room_->GetWidth() && currPos_.y >= room_->GetHeight())
+        velocity_ = sf::Vector2f(-ORC_SPEED, -ORC_SPEED);
+    if(currPos_.x < 0 && currPos_.y < 0) {
+        velocity_ = sf::Vector2f(ORC_SPEED, ORC_SPEED);
+    }
+    currPos_ += dt.asSeconds() * velocity_;
 }

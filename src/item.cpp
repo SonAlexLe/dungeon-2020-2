f8ababeab8707440bf2c6ff6e4a3084245444548
Kainulainen Antti <kainula3@lyta.org.aalto.fi>
2020-08-28 11:18:54 +0300

    Readded the drawing

--- src/item.cpp
+++ src/item.cpp
@@ -35,16 +35,23 @@ void Item::setEquipped() {
     equipped_ = true;
 }
 void Item::update(sf::Time dt) {
-    /*if (equipped_ == false && this->sprite_.getGlobalBounds().intersects(this->player_->GetSprite().getGlobalBounds())) { //Checks for collision with the player.
-        this->player_->GetInventory()->addItem();
+    if (equipped_ == false && this->sprite_.getGlobalBounds().intersects(this->player_->GetSprite().getGlobalBounds())) { //Checks for collision with the player.
         this->equipped_ = true;
-    }*/
+        this->player_->GetInventory()->addItem(shared_from_this());
+    }
 }
 
 void Item::draw(sf::RenderWindow* window) { //Now only draws the sprite but it also should draw the name of the item under it.
     if (this->equipped_ == false) { //Draw function should only draw the object if it's not equipped.
-        sprite_.setPosition(this->currPos_);
-        window->draw(this->sprite_);
+        sf::FloatRect m_rec = sprite_.getGlobalBounds();
+        sf::RectangleShape m_box(sf::Vector2f(m_rec.width, m_rec.height));
+        m_box.setOutlineThickness(2);
+        m_box.setOutlineColor(sf::Color::Transparent);
+        m_box.setFillColor(sf::Color::Transparent);
+        m_box.setPosition(sf::Vector2f(currPos_.x * 3, currPos_.y * 3));
+        window->draw(m_box);
+        sprite_.setPosition(sf::Vector2f(currPos_.x * 3, currPos_.y * 3));
+        window->draw(sprite_);
     }
 }
 
@@ -54,8 +61,7 @@ void Item::draw(sf::RenderWindow* window) { //Now only draws the sprite but it a
 // itemGenerator functions below.
 
 std::shared_ptr<Item> itemGenerator::createEquipment(float x, float y, std::shared_ptr<Player> player) { // Creates randomly an armor or a weapon for the item room.
-    srand((unsigned int)time(NULL));
-    int armorWeapon = rand() % 1 + 0;
+    int armorWeapon = (rand() % static_cast<int>(1 - 0 + 1));
     int quality = rand() % 2 + 0;
     int material = gameLvl_;
     int material_koht = gameLvl_ - 1;

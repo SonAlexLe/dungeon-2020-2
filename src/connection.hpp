d0a6d7b5ada51966e904217eac660b6456fe4dd9
rashid <rashid.jalloh@aalto.fi>
2020-08-28 11:31:31 +0300

    More cleanup on connections

--- src/connection.hpp
+++ src/connection.hpp
@@ -8,10 +8,9 @@
 #include "player.hpp"
 
 
-/*Each connection object functions as a doorway to a different room or new map 
-Connections inherit the Entity class, 
-on top of that each connection has a reference to another connection and a method for traversing from one connection to the other.
-By default connections are locked and need to be unlocked by key or by clearing the room (subject to change)
+/*Each connection object functions as a doorway to a different room
+The connections facing determines to which of the rooms neighbors the player is transported to.
+By default connections are locked and need to be unlocked by clearing the room 
 */
 class Connection : public Entity {
 public:
@@ -25,10 +24,8 @@ public:
 
     void update(sf::Time dt);
 
-    void load();
 
-    /*WIP. Maybe should take the player as a parameter or is called when a connection and a player collide.
-     Also needs to place the player on the correct spot after traversing. (Tried to connect connections together) */
+    //Moves the player to another room
     void traverse();
 
     void draw(sf::RenderWindow* window);
@@ -45,3 +42,4 @@ private:
 
     std::shared_ptr<Player> player_;
 };
+
--- src/room.hpp
+++ src/room.hpp
@@ -12,8 +12,8 @@ class Entity;
 class Connection;
 class Monster;
 
-/*
-
+/*Each room can have within it monsters, projectiles, connections and items.
+Besides these containers a room has pointers to its neighboring rooms and type-tag to tell if the room is special
 */
 class Room {
 public:
@@ -39,6 +39,7 @@ public:
 
     void RemoveProjectile(std::shared_ptr<Projectile>);
 
+    //A room is considered cleared when all monsters in it are inactive
     bool IsClear();
 
     std::string GetType();
